1313cc06be4378e763f1ea222a2fd50b84affa9d
Solomon Bonney <quophiclef@gmail>
2024-03-04 23:53:57 +0000

    cors origin url added

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://bosy-backend.vercel.app" }));
 
 //routes
 app.use("/api/todos", todoRoutes);
