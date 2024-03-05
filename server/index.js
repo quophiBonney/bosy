5e7a83ab7372b05b22d3b263e9ad046758f6553b
Solomon Bonney <quophiclef@gmail>
2024-03-05 03:14:39 +0000

    made the origin accessible by all sites

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({ origin: "https://api.whatsap.com" }));
+app.use(cors({ origin: "*" }));
 //routes
 app.use("/api/todos", todoRoutes);
 
