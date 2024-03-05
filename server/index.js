2e697d8d1978402be05a8bcab4a741398eb06f79
Solomon Bonney <quophiclef@gmail>
2024-03-05 01:45:19 +0000

    updated cors

--- server/index.js
+++ server/index.js
@@ -16,14 +16,12 @@ app.use(express());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const corsOptions = {
-  origin: "https://bosy.vercel.app",
-  methods: ["POST", "GET", "DELETE", "PUT"],
-  credentials: true, // Allow credentials (cookies, authorization headers)
-};
-
-// Apply CORS middleware with the specified options
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 //routes
 app.use("/api/todos", todoRoutes);
