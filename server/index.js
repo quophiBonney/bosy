703a4462199e3f3c13af138bb6427ca581b95c03
Solomon Bonney <quophiclef@gmail>
2024-03-05 02:33:21 +0000

    env connection string changed

--- server/index.js
+++ server/index.js
@@ -15,14 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
+app.use(cors());
 //routes
 app.use("/api/todos", todoRoutes);
 
